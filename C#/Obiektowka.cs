5fa60033963c749de18cfdaea4b660aeae9f78f7
Vex0on <73704968+Vex0on@users.noreply.github.com>
2021-12-23 16:24:30 +0100

    Update Obiektowka.cs

--- C#/Obiektowka.cs
+++ C#/Obiektowka.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +8,34 @@ namespace Cs_nauka
 {
     public class Pracownik
     {
-        public string nazwisko;
-        private double zarobki;
+        public static int liczbaPrac;
+        public string nazwisko { get; set; }
+        private double zarobki { get; set; }
 
         public Pracownik(String naz, double zar)
         {
+            liczbaPrac++;
             this.nazwisko = naz;
             this.zarobki = zar;
+        }
+
+        static Pracownik()
+        {
+            liczbaPrac = 0;
         }
         public void PokazPracownika()
         {
             Console.WriteLine("{0, -13} {1, 5}", nazwisko, zarobki);
+        }
+
+        public static double SumaZarobkow(Pracownik[] tab)
+        {
+            double suma = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                suma += tab[i].zarobki;
+            }
+            return suma;
         }
     }
 
@@ -27,8 +44,20 @@ namespace Cs_nauka
     {
         static void Main(string[] args)
         {
-            Pracownik p1 = new Pracownik("Malszewski", 2950);
-            p1.PokazPracownika();
+            Pracownik[] robota = new Pracownik[4];
+            robota[0] = new Pracownik("Malszewski", 2950.0);
+            robota[1] = new Pracownik("Kowalski", 3220.0);
+            robota[2] = new Pracownik("Wegierski", 2560.0);
+            robota[3] = new Pracownik("Olinowska", 3150.0);
+
+            foreach (Pracownik p in robota)
+            {
+                p.PokazPracownika();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Liczba Prac: {0}", Pracownik.liczbaPrac);
+            Console.WriteLine("Suma Zarobkow: {0}", Pracownik.SumaZarobkow(robota));
             Console.ReadKey();
         }
     }
