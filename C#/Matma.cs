c758e79f188b07940a3a63eefe483c6447069042
Vex0on <73704968+Vex0on@users.noreply.github.com>
2021-12-23 15:28:34 +0100

    Update Matma.cs

--- C#/Matma.cs
+++ C#/Matma.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,11 @@ namespace Cs_nauka
         static void Main(String[] args)
         {
             double x, y;
-
+            Console.WriteLine("Podaj x > 0: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Math.Sqrt(Math.Abs(Math.Sin(x)) * Math.Log(x, 2.0));
+            Console.WriteLine(y);
+            Console.ReadKey();
         }
     }
 }
