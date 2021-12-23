dcdd0ddadd6aaddeb6d9bd8ed99e3b607cb9533a
Vex0on <73704968+Vex0on@users.noreply.github.com>
2021-12-23 15:11:34 +0100

    Add files via upload

---
+++ C#/Konwersja_Typow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs_nauka
+{
+    class Konwersja_Typow
+    {
+        static void Main(String[] args)
+        {
+            const int komputery = 24;
+            int studenci;
+            double wynik;
+            Console.WriteLine("Podaj liczbę studentów: ");
+            studenci = Convert.ToInt32(Console.ReadLine());
+            wynik = (double)studenci / komputery;
+            Console.WriteLine(wynik);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Matma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs_nauka
+{
+    class Matma
+    {
+        static void Main(String[] args)
+        {
+            double x, y;
+
+        }
+    }
+}
