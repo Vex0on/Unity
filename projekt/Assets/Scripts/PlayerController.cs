e0ea9d3e0cda135787372ca02c0716a81dfccc1e
Vex0on <73704968+Vex0on@users.noreply.github.com>
2022-01-07 21:55:44 +0100

    Nowości w PlayerController i CameraController
    Teraz postać:
    - Porusza się stale w prawą stronę
    - Skacze gdy wciśniemy spację lub LPM
    - Może skakać jedynie pojedynczo, po uprzednim dotknięciu podłoża
    + Ma własną animację biegu i skoku
    
    A kamera:
    + Podąża za graczem

--- projekt/Assets/Scripts/PlayerController.cs
+++ projekt/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,13 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
 
     private Rigidbody2D myRigidbody;
-
+    
     public bool onGround;
     public LayerMask whatIsGround;
 
     private Collider2D myCollider;
+
+    private Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,8 @@ public class PlayerController : MonoBehaviour
         myRigidbody = GetComponent<Rigidbody2D>();
 
         myCollider = GetComponent<Collider2D>();
+
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -39,5 +43,9 @@ public class PlayerController : MonoBehaviour
                 myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
             }
         }
+        // Animacja biegu i skoku
+        myAnimator.SetFloat("Speed", myRigidbody.velocity.x);
+        myAnimator.SetBool("OnGround", onGround);
+
     }
 }
