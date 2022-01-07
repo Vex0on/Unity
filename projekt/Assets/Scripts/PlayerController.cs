19063815a05c2226071b6631df1b14d54145ed9b
Vex0on <73704968+Vex0on@users.noreply.github.com>
2022-01-07 04:44:45 +0100

    Dodatkowe funkcje w skrypcie PlayerController
    Teraz postać:
    - Porusza się stale w prawą stronę
    - Skacze gdy wciśniemy spację lub LPM
    - Może skakać jedynie pojedynczo, po uprzednim dotknięciu podłoża

--- projekt/Assets/Scripts/PlayerController.cs
+++ projekt/Assets/Scripts/PlayerController.cs
@@ -9,20 +9,35 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D myRigidbody;
 
+    public bool onGround;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Sprawdza czy postaæ dotyka Collidera czyli ziemi
+        onGround = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        // Stale biegn¹ca postaæ w praw¹ stronê
         myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
 
+        // W momencie wciœniêcia spacji lub LPM skaczemy z wartoœci¹ "jumpForce" tylko jeœli spe³niamy warunek onGround
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) )
         {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            if(onGround)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
         }
     }
 }
