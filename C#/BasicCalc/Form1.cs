fca6520859893128e1463aeb01643797116ad4f6
JSosnowski <vex0ono@gmail.com>
2022-03-19 14:10:06 +0100

    Decimal fix

--- C#/BasicCalc/Form1.cs
+++ C#/BasicCalc/Form1.cs
@@ -17,209 +17,202 @@ namespace BasicCalc
             InitializeComponent();
         }
 
-        float firstarg;
-        float secondarg;
-        float result = 0;
+        string firstarg = "";
+        string secondarg = "";
+        string input = "";
+        double result = 0;
         char func;
-        bool dec = false;
-
-        private void changeLabel(int numPressed)
-        {
-            if(dec == true)
-            {
-                int decCount = 0;
-                foreach (char c in CalcDisplay.Text)
-                {
-                    if (c == '.')
-                    {
-                        decCount++;
-                    }
-                }
-                if(decCount < 1)
-                {
-                    CalcDisplay.Text = CalcDisplay.Text + ".";
-                }
-                dec = false;
-            }
-            else
-            {
-                if (CalcDisplay.Text.Equals("0") == true && CalcDisplay.Text != null)
-                {
-                    CalcDisplay.Text = numPressed.ToString();
-                }
-                else if (CalcDisplay.Text.Equals("-0") == true)
-                {
-                    CalcDisplay.Text = "-" + numPressed.ToString();
-                }
-                else
-                {
-                    CalcDisplay.Text = CalcDisplay.Text + numPressed.ToString();
-                }
-            }
-        }
 
         private void oneButton_Click(object sender, EventArgs e)
         {
-            changeLabel(1);
+            CalcDisplay.Text = "";
+            input += "1";
+            CalcDisplay.Text += input;
         }
 
         private void twoButton_Click(object sender, EventArgs e)
         {
-            changeLabel(2);
+            CalcDisplay.Text = "";
+            input += "2";
+            CalcDisplay.Text += input;
         }
 
         private void threeButton_Click(object sender, EventArgs e)
         {
-            changeLabel(3);
+            CalcDisplay.Text = "";
+            input += "3";
+            CalcDisplay.Text += input;
         }
 
         private void fourButton_Click(object sender, EventArgs e)
         {
-            changeLabel(4);
+            CalcDisplay.Text = "";
+            input += "4";
+            CalcDisplay.Text += input;
         }
 
         private void fiveButton_Click(object sender, EventArgs e)
         {
-            changeLabel(5);
+            CalcDisplay.Text = "";
+            input += "5";
+            CalcDisplay.Text += input;
         }
 
         private void sixButton_Click(object sender, EventArgs e)
         {
-            changeLabel(6);
+            CalcDisplay.Text = "";
+            input += "6";
+            CalcDisplay.Text += input;
         }
 
         private void sevenButton_Click(object sender, EventArgs e)
         {
-            changeLabel(7);
+            CalcDisplay.Text = "";
+            input += "7";
+            CalcDisplay.Text += input;
         }
 
         private void eightButton_Click(object sender, EventArgs e)
         {
-            changeLabel(8);
+            CalcDisplay.Text = "";
+            input += "8";
+            CalcDisplay.Text += input;
         }
 
         private void nineButton_Click(object sender, EventArgs e)
         {
-            changeLabel(9);
+            CalcDisplay.Text = "";
+            input += "9";
+            CalcDisplay.Text += input;
         }
 
         private void zeroButton_Click(object sender, EventArgs e)
         {
-            changeLabel(0);
-            /*
             CalcDisplay.Text = "";
             input += "0";
             CalcDisplay.Text += input;
-            */
         }
 
         private void decimalButton_Click(object sender, EventArgs e)
         {
-            dec = true;
-            changeLabel(0);
+            input += ",";
+            CalcDisplay.Text += input;
+        }
+
+        private string GetSecondarg()
+        {
+            return secondarg;
         }
 
         private void equalButton_Click(object sender, EventArgs e)
         {
-            result = 0;
-            if(CalcDisplay.Text.Equals("0") == false)
-            {
-                //Plus
-                if (func == '+')
-                {
-                    secondarg = float.Parse(CalcDisplay.Text);
-                    result = firstarg + secondarg;
-                }
-                //Minus
-                else if (func == '-')
-                {
-                    secondarg = float.Parse(CalcDisplay.Text);
-                    result = firstarg - secondarg;
-                }
-                //Multiply
-                else if (func == '*')
-                {
-                    secondarg = float.Parse(CalcDisplay.Text);
-                    result = firstarg * secondarg;
-                }
-                //Divide
-                else if (func == '/')
-                {
-                    secondarg = float.Parse(CalcDisplay.Text);
-                    if (secondarg == '0')
-                    {
-                        CalcDisplay.Text = "Never divide by 0";
-                    }
-                    else
-                    {
-                        result = firstarg / secondarg;
-                    }
+            secondarg = input;
+            double firstNum, secondNum;
 
-                }
-                //Square
-                else if (func == '^')
+            //Plus
+            if (func == '+')
+            {
+                firstNum = Convert.ToDouble(firstarg);
+                secondNum = Convert.ToDouble(secondarg);
+                result = firstNum + secondNum;
+                CalcDisplay.Text = result.ToString();
+            }
+            //Minus
+            else if (func == '-')
+            {
+                firstNum = Convert.ToDouble(firstarg);
+                secondNum = Convert.ToDouble(secondarg);
+                result = firstNum - secondNum;
+                CalcDisplay.Text = result.ToString();
+            }
+            //Multiply
+            else if (func == '*')
+            {
+                firstNum = Convert.ToDouble(firstarg);
+                secondNum = Convert.ToDouble(secondarg);
+                result = firstNum * secondNum;
+                CalcDisplay.Text = result.ToString();
+            }
+            //Divide
+            else if (func == '/')
+            {
+                firstNum = Convert.ToDouble(firstarg);
+                secondNum = Convert.ToDouble(secondarg);
+                if (secondNum == '0')
                 {
-                    result = firstarg * firstarg;
+                    CalcDisplay.Text = "Never divide by 0";
+                }
+                else
+                {
+                    result = firstNum / secondNum;
+                    CalcDisplay.Text = result.ToString();
                 }
             }
-            CalcDisplay.Text = result.ToString();
-        }    
-            
+            //Square
+            else if (func == '^')
+            {
+                firstNum = Convert.ToDouble(firstarg);
+                result = firstNum * firstNum;
+                CalcDisplay.Text = result.ToString();
+            }
+            //Squareroot
+            else if (func == '$')
+            {
+                firstNum = Convert.ToDouble(firstarg);
+                result = Math.Sqrt(firstNum);
+                CalcDisplay.Text = result.ToString();
+            }
+        }
 
         private void minusButton_Click(object sender, EventArgs e)
         {
-            firstarg = float.Parse(CalcDisplay.Text);
             func = '-';
-            CalcDisplay.Text = "";
+            firstarg = input;
+            input = "";
         }
 
         private void plusButton_Click(object sender, EventArgs e)
         {
-            firstarg = float.Parse(CalcDisplay.Text);
             func = '+';
-            CalcDisplay.Text = "";
+            firstarg = input;
+            input = "";
         }
 
         private void multiplyButton_Click(object sender, EventArgs e)
         {
-            firstarg = float.Parse(CalcDisplay.Text);
             func = '*';
-            CalcDisplay.Text = "";
+            firstarg = input;
+            input = "";
         }
 
         private void divideButton_Click(object sender, EventArgs e)
         {
-            firstarg = float.Parse(CalcDisplay.Text);
             func = '/';
-            CalcDisplay.Text = "";
+            firstarg = input;
+            input = "";
         }
 
         private void squarerootButton_Click(object sender, EventArgs e)
         {
             func = '$';
-            firstarg = float.Parse(CalcDisplay.Text);
-            if (firstarg > 0)
-            {
-                double sqrt = Math.Sqrt(firstarg);
-                CalcDisplay.Text = sqrt.ToString();
-            }
+            firstarg = input;
+            input = "";
         }
 
         private void squareButton_Click(object sender, EventArgs e)
         {
-            firstarg = float.Parse(CalcDisplay.Text);
             func = '^';
-            CalcDisplay.Text = "";
+            firstarg = input;
+            input = "";
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
+            firstarg = "";
+            secondarg = "";
+            input = "";
+            result = 0.0;
             CalcDisplay.Text = "0";
-            firstarg = 0;
-            secondarg = 0;
-            result = 0;
-            func = '\0';
-            dec = false;
         }
     }
 }
